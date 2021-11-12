08ff4533984843811ead43be3edec1c04a4f2bfc
andyroi <91491675+andyroi@users.noreply.github.com>
2021-11-12 16:23:47 -0700

    Add files via upload

---
+++ Assignment7.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class Assignment7 {
+  public static void main(String[] args ){
+
+	System.out.println(" ****** Start of Program ******");
+	printMenu();
+	Scanner scan = new Scanner (System.in);
+	char command;  		// command letter
+    CardList myCards = new CardList (); //constructor
+
+
+	do{
+        System.out.print("Please input a commad or type ?: ");
+		command = scan.nextLine().toLowerCase().charAt(0);
+
+		switch(command)
+		{
+			case 'a': //add the cards and the amount of cards
+				System.out.println("a [Create new cards]" );
+                System.out.print("[Input the size of cards]" );
+				int sizecards = scan.nextInt();
+				myCards.CardList(sizecards);
+				System.out.println(myCards.getHistory());
+				break;
+			case 'b': //flip the order
+				System.out.print("b [Flip the cards]" );
+				System.out.print("\n\t [Type a question]: " );
+				break;
+			case 'c': //shift to the left
+				System.out.print("c [shift the cards]" );
+				break;
+			case 'd'://randomize array position
+			System.out.println("d [Shuffle the cards]" );
+			break;
+			case 'e'://change the current cards and give new ones 
+				System.out.print("e [Change the cards]" );
+				break;
+			case '?': 
+				printMenu(); 
+				break;
+			case 'q': 
+				System.out.println("");
+				break;
+			default: System.out.print("\n\t Invalid: *** Type '?' to get the commands***"); break;
+		}
+		System.out.println("\n");
+	}while(command != 'q');
+	 System.out.println(" ****** End of Program ******");
+} // end of main ()
+
+   // Answer the
+   // Display the command menu
+   public static void printMenu(){
+		System.out.println();
+		System.out.println("Command Options");
+        System.out.println("--------------------------------");
+        System.out.println("a. Create new cards");
+        System.out.println("b. Flip the cards");
+        System.out.println("c. Shift to end");
+        System.out.println("d. Shuffle cards ");
+        System.out.println("e. Change the cards");
+        System.out.println("?. Display the menu again");
+        System.out.println("q. Quit the program");
+	}
+}
---
+++ CardList.java
@@ -0,0 +1,122 @@
+//************************************************************
+// Name : Andrew Rodriguez
+// Title : Assignment7.java & CardList.java
+// Description: The program will ask the user the amount of cards, and the the user will be given random cards, with the inputted amount, then they'll have options, to create new cards, flip, shift, shuffle, change, display, and quit the program. This HW practices private classes and mainly arrays.
+// Time:
+// Date: 11/10/21
+//*************************************************************
+/*** 
+    a)public int[] swapEnds(int[] nums) {
+        int temp = nums[0];
+         nums[0] = nums[nums.length-1];
+        nums[nums.length-1] = temp;
+        return nums;
+    } 
+    b)public boolean commonEnd(int[] a, int[] b) {
+    return (a[0] == b[0] || a[a.length -1] == b[b.length-1]);
+    }
+    c)public int countEvens(int[] nums) {
+        int count = 0;
+        for (int i  = 0; i < nums.length; i++)
+        {
+            if (nums[i] % 2 == 0)
+            {
+            count++;
+            }
+        }
+        return count;
+    }
+    d)public int sum13(int[] nums) {
+        int sum = 0;
+        for (int i= 0; i < nums.length; i++)
+        {
+            if(nums[i] == 13)
+            {
+            nums[i] = 0;
+                if(i == nums.length-1)
+                {
+                    if(nums[nums.length-1] == 13)
+                    {
+                    nums[i] = 0;
+                    }
+                }
+                else
+                {
+                    nums[i + 1] = 0;
+                }
+            }
+            sum += nums[i];
+        }
+    return sum;
+    }
+    e)public int[] fix34(int[] nums) {
+        for(int i = 0; i < nums.length; i++)
+        {
+            for(int j = 0; j < nums.length; j ++)
+            {
+                if (nums[i] == 4 && nums[j] == 3)
+                {
+                    int temp = nums[j +1];
+                    nums[j+1] = nums[i];
+                    nums[i] = temp;
+                 }
+            }
+        }
+    return nums;
+    }
+*/
+public class CardList {
+    private char[] cards;
+    private String history = "";
+    private String type = "";
+    public void setCard()
+    {
+        this.cards = cards;
+    }
+    public CardList(int numberOfCard)
+    {
+        //this.cards = numberOfCard;      
+        /*cards = new char[numberOfCard];
+		String randomcards = "A234567890JQK";
+		for (int i = 0; i < numberOfCard; i++)
+		{
+			cards[i] = randomcards.charAt((int)(Math.random()*13)); 
+		}
+        */
+    }
+    public void flip()
+    {
+        for(int i = cards.length-1; i >= 0; i--)
+        {
+            int start = 0;
+            int end = cards.length-1;
+
+        }
+        type = ":Flip";
+    }
+    public void shift()
+    {
+
+    }
+    public void shuffle()
+    {
+
+    }
+    public void change(int size)
+    {
+
+    }
+    //private int[] shuffleIndex()
+    //{
+        
+    //}
+    public String getHistory()
+    {
+        for(int i = 0; i < cards.length; i++)
+        {
+            history += cards[i] + " , ";
+        }
+        return history;
+    }
+
+}
